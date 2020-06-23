bcfdf0ee4452139a9f4df1fe69afd78f9f135fb6
kxdebian <kxdebian@outlook.jp>
2020-06-23 20:29:50 +0900

    test

--- lcopy.sh
+++ lcopy.sh
@@ -15,7 +15,7 @@ mkdir -p $var_destdir
 
 while read var_source; do
     var_destination=${var_destdir}${var_source}
-    cp --archive --parents --no-dereference --preserve ${var_source} ${var_destdir}
+    cp --archive --parents --no-dereference ${var_source} ${var_destdir}
 done << END
     $var_sourceList
 END
